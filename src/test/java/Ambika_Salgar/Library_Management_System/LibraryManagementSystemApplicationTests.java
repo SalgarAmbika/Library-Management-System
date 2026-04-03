e73d876fd3f007aaf2d7296be942c58792ba48f8
Shrihake <shriramhakedbatu@gmail.com>
2026-04-03 18:22:40 +0530

    Initial commit

---
+++ src/test/java/Ambika_Salgar/Library_Management_System/LibraryManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package Ambika_Salgar.Library_Management_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
